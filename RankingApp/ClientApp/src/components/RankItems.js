0b10e25aaf9c2160ff4a318cd491d3410af93de3
NirajMestry <76642650+iam-niraj@users.noreply.github.com>
2023-11-09 10:03:57 +0530

    Add project files.

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,49 @@
+﻿import { useEffect, useState } from 'react';
+import ItemCollection from './ItemCollection';
+import MovieImageArr from './MovieImages';
+import RankingGrid from './RankingGrid';
+
+const RankItems = () => {
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((res) => {
+                return res.json();
+            }).then(data => { setItems(data) })
+    }, [])
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+
+        ev.preventDefault();
+        const targetElm = ev.target;
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+                { ...item, ranking: parseInt(targetElm.id.substring(5)) } : { ...item, ranking: item.ranking });
+            setItems(transformedCollection);
+        }
+
+    }
+
+    return (
+        < main >
+            <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop} />
+            <ItemCollection items={items} drag={drag} imgArr={MovieImageArr} />
+
+        </main>)
+}
+
+export default RankItems;
